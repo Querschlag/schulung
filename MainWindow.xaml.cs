afda52aae6fb727a8595624eb8b02e16c35a6abb
AlterSack <altjohannes510@googlemail.com>
2014-03-26 19:14:31 +0100

    changes in ui

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,97 +39,68 @@ namespace Schulung
             // create game class
             _run = new Run(Points);
 
-            // set maximum of country bar
-            this.SliderCountry.Maximum = Points;
-
-            // set maximum of economy bar
-            this.SliderEconomy.Maximum = Points;
-
-            // set maximum of terror bar
-            this.SliderTerror.Maximum = Points;
-
-            // set maximum of personal bar
-            this.SliderResearch.Maximum = Points;
-            
-            // calculate rest point
-            CalculateRestPoints();
+            // change points
+            OnChangePoints();
         }
 
         /// <summary>
-        ///  method called on slider country value changed
+        ///  method called on changed points
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void SliderCountry_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        public void OnChangePoints()
         {
-            // set text block
-            this.LabelCountry.Text = this.SliderCountry.Value.ToString();
+            // set maximum points for country
+            this.SetMaximumCountry();
 
-            // calculate rest points
-            CalculateRestPoints();
+            // set maximum points for economy
+            this.SetMaximumEconomy();
+
+            // set maximum points for terror
+            this.SetMaximumTerror();
+
+            // set maximum points for research
+            this.SetMaximumResearch();
+
+            // calculate sum
+            int sum = this.PointsCountry.Value + this.PointsEconomy.Value + this.PointsTerror.Value + this.PointsResearch.Value;
+
+            // auto disable or enable next round button
+            this.ButtonStart.IsEnabled = sum == Points;
         }
 
         /// <summary>
-        ///  method to calculate rest point
+        ///  method to set maximum points for country
         /// </summary>
-        private void CalculateRestPoints()
+        private void SetMaximumCountry()
         {
-            // get used points
-            int used = 0;
-
-            // increase used points
-            used += (int)this.SliderCountry.Value;
-            used += (int)this.SliderEconomy.Value;
-            used += (int)this.SliderTerror.Value;
-            used += (int)this.SliderResearch.Value;
-
-            // enable or disable button
-            this.ButtonStart.IsEnabled = used == Points;
-            
-            // set points
-            this.LabelPoints.Text = (Points - used).ToString();
+            // set maximum
+            this.PointsCountry.Maximum = Points - (this.PointsEconomy.Value + this.PointsTerror.Value + this.PointsResearch.Value);
         }
 
         /// <summary>
-        ///  method called on slider economy value changed
+        ///  method to set maximum points for economy
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void SliderEconomy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void SetMaximumEconomy()
         {
-            // set text block
-            this.LabelEconomy.Text = this.SliderEconomy.Value.ToString();
-
-            // calculate rest points
-            CalculateRestPoints();
+            // set maximum
+            this.PointsEconomy.Maximum = Points - (this.PointsCountry.Value + this.PointsTerror.Value + this.PointsResearch.Value);
         }
 
         /// <summary>
-        ///  method called on slider terror value changed
+        ///  method to set maximum points for terror
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void SliderTerror_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void SetMaximumTerror()
         {
-            // set text block
-            this.LabelTerror.Text = this.SliderTerror.Value.ToString();
-
-            // calculate rest points
-            CalculateRestPoints();
+            // set maximum
+            this.PointsTerror.Maximum = Points - (this.PointsCountry.Value + this.PointsEconomy.Value + this.PointsResearch.Value);
         }
 
         /// <summary>
-        ///  method called on slider personal value changed
+        ///  method to set maximum points for research
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void SliderResearch_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void SetMaximumResearch()
         {
-            // set text block
-            this.LabelResearch.Text = this.SliderResearch.Value.ToString();
-
-            // calculate rest points
-            CalculateRestPoints();
+            // set maximum
+            this.PointsResearch.Maximum = Points - (this.PointsCountry.Value + this.PointsEconomy.Value + this.PointsTerror.Value);
         }
 
         /// <summary>
@@ -140,7 +111,7 @@ namespace Schulung
         private void ButtonStart_Click(object sender, RoutedEventArgs e)
         {
             // call main
-            _run.Main((int)this.SliderCountry.Value, (int)this.SliderEconomy.Value, (int)this.SliderTerror.Value, (int)this.SliderResearch.Value);
+            // _run.Main((int)this.SliderCountry.Value, (int)this.SliderEconomy.Value, (int)this.SliderTerror.Value, (int)this.SliderResearch.Value);
         }
     }
 }
---
+++ PointsControl.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Schulung
+{
+    /// <summary>
+    ///  event delegate for the value change event
+    /// </summary>
+    public delegate void OnValueChanged();
+
+    /// <summary>
+    /// Interaktionslogik für Points.xaml
+    /// </summary>
+    public partial class PointsControl : UserControl
+    {
+        /// <summary>
+        ///  storage for the value
+        /// </summary>
+        private int _value = 0;
+
+        /// <summary>
+        ///  storage for the value
+        /// </summary>
+        public int Value
+        {
+            get { return this._value; }
+            private set
+            {
+                // store value
+                this._value = value;
+
+                // initialize points control new
+                Init();
+            }
+        }
+
+        /// <summary>
+        ///  storage for the maximum
+        /// </summary>
+        private int _maximum = 0;
+
+        /// <summary>
+        ///  storage for the maximum
+        /// </summary>
+        public int Maximum 
+        {
+            get { return this._maximum; }
+            set
+            {
+                // store value
+                this._maximum = value;
+                
+                // initialize points control new
+                Init();
+            }
+        }
+
+        /// <summary>
+        ///  default constructor
+        /// </summary>
+        public PointsControl()
+        {
+            // initialize components
+            InitializeComponent();
+
+            // initialize points control new
+            Init();
+        }
+
+        /// <summary>
+        ///  event for the value changed event handler
+        /// </summary>
+        public event OnValueChanged ValueChanged;
+
+        /// <summary>
+        ///  method called on button plus clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+            // check value
+            if ((Value + 1) <= Maximum)
+            {
+                // increase value
+                Value++;
+
+                // check and raise event handler
+                if (this.ValueChanged != null) this.ValueChanged();
+            }
+        }
+
+        /// <summary>
+        ///  method called on button minus clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            // check value
+            if ((Value - 1) >= 0)
+            {
+                // decrease value
+                Value--;
+
+                // check and raise event handler
+                if (this.ValueChanged != null) this.ValueChanged();
+            }
+        }
+
+        /// <summary>
+        ///  method to initialize points control new
+        /// </summary>
+        private void Init()
+        {
+            // auto disable or enable buttons
+            this.ButtonMinus.IsEnabled = Value > 0;
+            this.ButtonPlus.IsEnabled = Value < Maximum;
+
+            // set value
+            this.LabelPoints.Text = Value.ToString("0");
+        }
+
+        /// <summary>
+        ///  method to reset all
+        /// </summary>
+        public void Reset()
+        {
+            // reset value
+            Value = 0;
+
+            // reset maximum
+            Maximum = 0;
+        }
+    }
+}
